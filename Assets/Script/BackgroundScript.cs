2cd489890f39cc70443ecee7fbe98475c7fd90d4
Thiago Yonamine <thyonamine1@gmail.com>
2020-01-18 23:44:33 -0300

    bg

---
+++ Assets/Script/BackgroundScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+
+    public Camera mainCamera;
+
+    private GameObject ceu;
+    private GameObject solo3;
+    private GameObject solo2;
+    private GameObject solo1;
+
+
+    public void Start()
+    {
+        solo1 = this.transform.GetChild(1).gameObject;
+        solo2 = this.transform.GetChild(2).gameObject;
+        solo3 = this.transform.GetChild(3).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = mainCamera.transform.position.x;
+        float y = mainCamera.transform.position.y;
+
+        solo1.transform.position = new Vector3(x/1.1f, -y-6f,0);
+        solo2.transform.position = new Vector3(x/1.2f, -y/1.1f-1, 0);
+        solo3.transform.position = new Vector3(x/1.5f, -y/1.5f, 0);
+    }
+}
--- Assets/Script/CameraScript.cs
+++ Assets/Script/CameraScript.cs
@@ -13,14 +13,33 @@ public class CameraScript : MonoBehaviour
     public float scaleY = 0.2f;
     public float cameraSize = 5;
     public float offsetY = 1.2f;
+    public float offsetX = 0f;
+
+    public float initialSize;
+
+    void Start()
+    {
+        float resolution = Screen.width / (Screen.height * 1.0F);
+        cameraSize *= (2F/ resolution);
+
+        cameraSpeed = 0.1f;
+    }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 playerPosition = player.GetComponent<Transform>().position;
         float currentCameraSize = this.GetComponent<Camera>().orthographicSize;
+
+        if (cameraSpeed < 2f)
+        {
+            cameraSpeed *= 1.05f;
+            Debug.Log("cameraSize: " + cameraSpeed);
+        }
         this.GetComponent<Camera>().orthographicSize = Mathf.Lerp(currentCameraSize, cameraSize+playerPosition.y*0.05f, Time.deltaTime*cameraSpeed);
-        desireX = playerPosition.x * scaleX;
+
+
+        desireX = (playerPosition.x * scaleX)-offsetX;
         desireY = (playerPosition.y * scaleY)-offsetY;
         Vector3 currentPosition = this.GetComponent<Transform>().position;
         if (Mathf.Abs(currentPosition.x - desireX) > 0.1 || Mathf.Abs(currentPosition.y - desireY) > 0.1)
