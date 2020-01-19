b3e985b40dbdddf79a309732f001630a667c6cf0
JessicaDias <jcarolinedias1@gmail.com>
2020-01-19 02:42:12 -0300

    Fase21

--- Assets/Script/Constant.cs
+++ Assets/Script/Constant.cs
@@ -8,9 +8,9 @@ public static class Constant
     public static readonly string FASE_PREFIX = "Fase";
     public static readonly string EXTRA_BOMBS = "Extra";
 
-    //                               Level 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20
-    public static readonly int[] initb = { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
-    public static readonly int[] star3 = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
-    public static readonly int[] star2 = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-    public static readonly int[] star1 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    //                               Level 1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21
+    public static readonly int[] initb = { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 6};
+    public static readonly int[] star3 = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0};
+    public static readonly int[] star2 = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    public static readonly int[] star1 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2};
 }
